3b489fed2d7825cc312336d3cd5a3eeda2a7f37d
SivaSundar17 <97680885+SivaSundar17@users.noreply.github.com>
2025-04-12 00:37:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
   public static void main(String args[]){
     //adding a comment
-    System.out.println("Hello world program to demonstrate jenkins pipeline");
+    System.out.println("Hello world program to demonstrate jenkins pipeline")
   }
 }
